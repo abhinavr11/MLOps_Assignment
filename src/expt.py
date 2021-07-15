5af3e718e7efa026d2ab8bb3d2bb690bafcb59d9
abhinavr11 <abhinav.raghuvanshi2.ar@gmail.com>
2021-07-15 17:35:00 +0530

    saving file in main

--- src/expt.py
+++ src/expt.py
@@ -3,7 +3,6 @@ import dvc.api
 x = dvc.api.get_url(repo ="https://github.com/abhinavr11/MLOps_Assignment", path = "data/creditcard.csv" )
 print(x)
 
-with dvc.api.open(repo ="https://github.com/abhinavr11/MLOps_Assignment", path = "data/creditcard.csv" , mode = "r" , rev = "exp") as fd:
+with dvc.api.open(repo ="https://github.com/abhinavr11/MLOps_Assignment", path = "data/creditcard.csv" , mode = "r" ) as fd:
     df = pd.read_csv(fd)
 
-    
