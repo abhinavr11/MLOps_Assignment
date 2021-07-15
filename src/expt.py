4cf179df81b6374acd97d3f96ee15a62cdbe86d0
abhinavr11 <abhinav.raghuvanshi2.ar@gmail.com>
2021-07-15 17:16:33 +0530

    final test code

--- src/expt.py
+++ src/expt.py
@@ -2,3 +2,6 @@ import pandas as pd
 import dvc.api
 x = dvc.api.get_url(repo ="https://github.com/abhinavr11/MLOps_Assignment", path = "data/creditcard.csv" )
 print(x)
+
+with dvc.api.open(repo ="https://github.com/abhinavr11/MLOps_Assignment", path = "data/creditcard.csv" , mode = "r" , rev = "exp") as fd:
+    df = pd.read_csv(fd)
